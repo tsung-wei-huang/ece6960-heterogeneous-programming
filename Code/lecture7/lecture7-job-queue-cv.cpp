c902c92542d1ee5fa141b9e8d81696e888afb44a
Tsung-Wei Huang <twh760812@gmail.com>
2023-02-06 14:56:09 -0700

    updated lecture7

--- Code/lecture7/lecture7-job-queue-cv.cpp
+++ Code/lecture7/lecture7-job-queue-cv.cpp
@@ -6,6 +6,7 @@
 #include <queue>
 #include <functional>
 #include <mutex>
+#include <condition_variable>
 
 template <typename T>
 struct MoC {
@@ -22,26 +23,42 @@ int main(int argc, char* argv[]) {
 
   std::mutex mtx;
   std::vector<std::thread> threads;
-  std::vector<std::promise<void>> promises(1000);
   std::vector<std::future<void>> futures;
+  std::condition_variable cv;
   
   // stop signal sent by the main thread
-  //bool stop = false;
-  std::atomic<bool> stop {false};
+  std::atomic<bool> stop = false;
   std::queue< std::function<void()> > queue;
 
   for(int i=0; i<4; i++) {
-    threads.emplace_back([&mtx, &queue, &stop](){
+    threads.emplace_back([&mtx, &cv, &queue, &stop](){
       // keep doing my job until the main thread sends a stop signal
       while(!stop) {
         std::function<void()> task;
         // my job is to iteratively grab a task from the queue
-        mtx.lock();
-        if(queue.empty() == false) {
-          task = queue.front();
-          queue.pop();
+        {
+          // this version is pretty bad because it forces all threads
+          // to stay in a busy loop of getting tasks from the queue ...
+          // and... we know most of the time the queue is empty ...
+          //std::scoped_lock lock(mtx);
+          //if(queue.empty() == false) {
+          //  task = queue.front();
+          //  queue.pop();
+          //}
+
+          // 
+          std::unique_lock lock(mtx);
+          while(queue.empty() && !stop) {
+
+            // TODO: bug here... the thread may miss the notification
+
+            cv.wait(lock);
+          }
+          if(!queue.empty()) {
+            task = queue.front();
+            queue.pop();
+          }
         }
-        mtx.unlock();
         // and run the task...
         if(task) {
           task();
@@ -52,43 +69,23 @@ int main(int argc, char* argv[]) {
 
   // main thread insert 1000 tasks into the queue
   for(int i=0; i<1000; i++) {
-    //futures.emplace_back(promises[i].get_future());
-    //mtx.lock();
-    //queue.push(
-    //  [i, &p=promises[i]] () mutable {
-    //    printf("task %d finished by thread %p\n", i, std::this_thread::get_id());
-    //    p.set_value();
-    //  }
-    //);
-    //mtx.unlock();
-    
-    // std::function requires the target to be copyable, so
-    // we cannot just use the plain std::promise which is move-only
-    //auto promise = std::make_shared<std::promise<void>>();
-    //futures.emplace_back(promise->get_future());
-    //mtx.lock();
-    //queue.push(
-    //  [i, promise] () mutable {
-    //    printf("task %d finished by thread %p\n", i, std::this_thread::get_id());
-    //    promise->set_value();
-    //  }
-    //);
-    //mtx.unlock();
-
     std::promise<void> promise;
     futures.emplace_back(promise.get_future());
-    mtx.lock();
-    queue.push(
-      [i, moc=MoC{std::move(promise)}] () mutable {
-        printf("task %d finished by thread %p\n", i, std::this_thread::get_id());
-        moc.object.set_value();
-      }
-    );
-    mtx.unlock();
+    {
+      std::scoped_lock lock(mtx);
+      queue.push(
+        [i, moc=MoC{std::move(promise)}] () mutable {
+          printf("task %d finished by thread %p\n", i, std::this_thread::get_id());
+          moc.object.set_value();
+        }
+      );
+    }
+    cv.notify_one();
+
+    // do something else...
+    //std::this_thread::sleep_for(std::chrono::seconds(1));
   }
 
-  // TODO: how does the main thread know when the 1000 tasks finish 
-  //       and send the stop signal (stop = true)
   // Solution: main thread wait until all futures become available,
   //           i.e., the corresponding promises have been carried out
   //           by four threads
@@ -98,6 +95,7 @@ int main(int argc, char* argv[]) {
 
   // now, I know all the 1000 tasks finish, so I can stop the job queue
   stop = true;
+  cv.notify_all();
   
   // I need to join the threads to release their resources
   for(auto& t : threads) t.join();
