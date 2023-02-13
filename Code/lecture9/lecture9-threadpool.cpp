f41ff58f51029d21d4e496a6714dc76ac3633cc1
Tsung-Wei Huang <twh760812@gmail.com>
2023-02-13 14:49:54 -0700

    updated threadpool

--- Code/lecture9/lecture9-threadpool.cpp
+++ Code/lecture9/lecture9-threadpool.cpp
@@ -7,6 +7,7 @@
 #include <functional>
 #include <mutex>
 #include <condition_variable>
+#include <type_traits>
 
 template <typename T>
 struct MoC {
@@ -30,29 +31,32 @@ class Threadpool {
     // constructor tasks a unsigned integer representing the number of
     // workers you need
     Threadpool(size_t N) {
-      threads.emplace_back([this](){
-        // keep doing my job until the main thread sends a stop signal
-        while(!stop) {
-          std::function<void()> task;
-          // my job is to iteratively grab a task from the queue
-          {
-            // Best practice: anything that happens inside the while continuation check
-            // should always be protected by lock
-            std::unique_lock lock(mtx);
-            while(queue.empty() && !stop) {
-              cv.wait(lock);
+
+      for(size_t i=0; i<N; i++) {
+        threads.emplace_back([this](){
+          // keep doing my job until the main thread sends a stop signal
+          while(!stop) {
+            std::function<void()> task;
+            // my job is to iteratively grab a task from the queue
+            {
+              // Best practice: anything that happens inside the while continuation check
+              // should always be protected by lock
+              std::unique_lock lock(mtx);
+              while(queue.empty() && !stop) {
+                cv.wait(lock);
+              }
+              if(!queue.empty()) {
+                task = queue.front();
+                queue.pop();
+              }
             }
-            if(!queue.empty()) {
-              task = queue.front();
-              queue.pop();
+            // and run the task...
+            if(task) {
+              task();
             }
           }
-          // and run the task...
-          if(task) {
-            task();
-          }
-        }
-      });
+        });
+      }
     }
 
     // destructor will release all threading resources by joining all of them
@@ -72,7 +76,45 @@ class Threadpool {
 
     // insert a task "callable object" into the threadpool
     template <typename C>
-    auto insert(C task) {
+    auto insert(C&& task) {
+      std::promise<void> promise;
+      auto fu = promise.get_future();
+      {
+        std::scoped_lock lock(mtx);
+        queue.push(
+          [moc=MoC{std::move(promise)}, task=std::forward<C>(task)] () mutable {
+            task();
+            moc.object.set_value();
+          }
+        );
+      }
+      cv.notify_one();
+      return fu;
+    }
+    
+    // insert a task "callable object" into the threadpool
+    template <typename C>
+    auto insert_with_return(C&& task) {
+      using R = std::result_of_t<C()>;
+      std::promise<R> promise;
+      auto fu = promise.get_future();
+      {
+        std::scoped_lock lock(mtx);
+        queue.push(
+          [moc=MoC{std::move(promise)}, task=std::forward<C>(task)] () mutable {
+            moc.object.set_value(
+              task()
+            );
+          }
+        );
+      }
+      cv.notify_one();
+      return fu;
+    }
+    
+    // insert a task "callable object" into the threadpool using a generic
+    // function wrapper (instead of a template argument)
+    auto insert_2(std::function<void()> task) {
 
       std::promise<void> promise;
       auto fu = promise.get_future();
@@ -80,7 +122,7 @@ class Threadpool {
       {
         std::scoped_lock lock(mtx);
         queue.push(
-          [moc=MoC{std::move(promise)}, task] () mutable {
+          [moc=MoC{std::move(promise)}, task=std::move(task)] () mutable {
             task();
             moc.object.set_value();
           }
@@ -112,7 +154,9 @@ int main(int argc, char* argv[]) {
   std::vector<std::future<void>> futures;
   
   // From application's perspective:
-  Threadpool threadpool(4);
+
+  // create a thread pool of the maximum hardware concurrency
+  Threadpool threadpool(std::thread::hardware_concurrency());
   
   // insert 1000 tasks into the threadpool
   for(int i=0; i<1000; i++) {
@@ -125,6 +169,15 @@ int main(int argc, char* argv[]) {
 
   // do something while waiting for the threadpool (a set of worker threads) to
   // finish all the 1000 tasks ...
+  
+  // TODO: finish the method insert_with_return
+  std::future<int> fu_int = threadpool.insert_with_return([](){
+    return 10;
+  });
+
+  std::future<std::string> fu_string = threadpool.insert_with_return([](){
+    return std::string("hi");
+  });
 
   // now it's time to synchronize on the 1000 tasks
   for(auto& fu : futures) {
