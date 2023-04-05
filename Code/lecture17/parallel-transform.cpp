6cb6c5067bc9f0cc3ddebd126a44df29bcd1fe4a
Tsung-Wei Huang <twh760812@gmail.com>
2023-04-05 12:38:20 -0600

    updated lecture16

--- Code/lecture17/parallel-transform.cpp
+++ Code/lecture17/parallel-transform.cpp
@@ -121,6 +121,34 @@ class Threadpool {
       }
     }
 
+    template <typename SrcItr, typename DesItr, typename F>
+    void transform(SrcItr first1, SrcItr last1, DesItr first2, F uop, size_t chunk_size = 1) {
+
+      // the total number of elements in the range [beg, end)
+      size_t N = std::distance(first1, last1);
+
+      std::vector<std::future<void>> futures;
+      std::atomic<size_t> takens {0};
+
+      for(size_t i=0; i<threads.size(); i++) {
+        futures.emplace_back(insert([=, &takens](){
+          size_t curr_b = takens.fetch_add(chunk_size, std::memory_order_relaxed);              
+          while(curr_b < N) {
+            size_t curr_e = std::min(N, curr_b + chunk_size);
+            // apply func to the range specified by beg + [curr_b, curr_e)
+            std::transform(first1 + curr_b, first1 + curr_e, first2 + curr_b, uop);
+            // get the next chunk
+            curr_b = takens.fetch_add(chunk_size, std::memory_order_relaxed);              
+          }
+        }));
+      }
+      
+      // caller thread to wait for all W tasks finish (futures)
+      for(auto & fu : futures) {
+        fu.get();
+      }
+    }
+
     template <typename Input, typename T, typename F>
     T reduce(Input beg, Input end, T init, F bop, size_t chunk_size = 2) {
       size_t N = std::distance(beg, end);
@@ -170,6 +198,63 @@ class Threadpool {
       
       return init;
     }
+    
+    template <typename Input1, typename Input2, typename T, typename R, typename B>
+    T transform_reduce(Input1 beg1, Input end1, Input2 beg2, T init, R rop, B top, size_t chunk_size = 2) {
+
+      size_t N = std::distance(beg, end);
+
+      std::vector<std::future<void>> futures;
+      std::atomic<size_t> takens {0};
+
+      std::mutex mutex;
+      
+      for(size_t i=0; i<threads.size(); i++) {
+        futures.emplace_back(insert([=, &init, &mutex, &takens](){
+          // pre-reduce
+          size_t curr_b = takens.fetch_add(2, std::memory_order_relaxed);
+          
+          // corner case #1: no more elements to reduce
+          if(curr_b >= N) {
+            return;
+          }
+          // corner case #2: only one element left
+          if(N - curr_b == 1) {
+            std::scoped_lock lock(mutex);
+            init = rop(init, top(*(beg1 + curr_b), *(beg2 + curr_b)));
+            return;
+          }
+
+          // perform a reduction on these two elements
+          T temp = rop( 
+            top(*(beg1 + curr_b), *(beg2 + curr_b)), 
+            top(*(beg1 + curr_b+1), *(beg2 + curr_b + 1))
+          );
+          curr_b = takens.fetch_add(chunk_size, std::memory_order_relaxed);              
+          while(curr_b < N) {
+            size_t curr_e = std::min(N, curr_b + chunk_size);
+            // run a sequential reduction to the range specified by beg + [curr_b, curr_e)
+            temp = std::transform_reduce(
+              beg1 + curr_b, beg1 + curr_e, beg2 + curr_b, temp, rop, top
+            );
+            // get the next chunk
+            curr_b = takens.fetch_add(chunk_size, std::memory_order_relaxed);              
+          }
+          // perform a final reduction on temp with init
+          {
+            std::scoped_lock lock(mutex);
+            init = rop(init, temp);
+          }
+        }));
+      }
+      
+      // caller thread to wait for all W tasks finish (futures)
+      for(auto & fu : futures) {
+        fu.get();
+      }
+      
+      return init;
+    }
 
   private:
 
@@ -193,6 +278,25 @@ auto par_reduce(std::vector<int>& vec, Threadpool& threadpool) {
   );
 }
 
+auto seq_transform(std::vector<int>& src, std::vector<std::string>& des) {
+  std::transform(
+    src.begin(), src.end(), des.begin(),
+    [](int v){
+      return std::to_string(v) + " is the result";
+    }
+  );
+}
+
+auto par_transform(std::vector<int>& src, std::vector<std::string>& des, Threadpool& threadpool) {
+  threadpool.transform(
+    src.begin(), src.end(), des.begin(),
+    [](int v){
+      return std::to_string(v) + " is the result";
+    },
+    1024
+  );
+}
+
 int main(int argc, char* argv[]) {
 
   // usage: ./a.out T N
@@ -207,26 +311,29 @@ int main(int argc, char* argv[]) {
   // create a thread pool of the maximum hardware concurrency
   Threadpool threadpool(T);
 
-  std::vector<int> vec(N);
-  for(auto& i : vec) {
-    i = 1;
+  std::vector<int> src(N);
+  for(auto& i : src) {
+    i = ::rand() % 10;
   }
-  
+
+  std::vector<std::string> des(N);
+
   // run reduce sequentially
-  std::cout << "running seq_reduce ... ";
+  std::cout << "running seq_transform ... ";
   auto beg = std::chrono::steady_clock::now();
-  auto res1 = seq_reduce(vec);
+  seq_transform(src, des);
   auto end = std::chrono::steady_clock::now();
   std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end-beg).count()
             << "ns\n"; 
 
   // run reduce parallely
-  std::cout << "running par_for_each ... ";
+  std::cout << "running par_transform ... ";
   beg = std::chrono::steady_clock::now();
-  auto res2 = par_reduce(vec, threadpool);
+  par_transform(src, des, threadpool);
   end = std::chrono::steady_clock::now();
   std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end-beg).count()
             << "ns\n"; 
+  
 
   // shut down the threadpool
   threadpool.shutdown();
